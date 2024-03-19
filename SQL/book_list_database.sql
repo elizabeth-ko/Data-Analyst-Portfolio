f7627b09de49759cb7a9abd74eb869bcf2a9ff24
elizabeth-ko <163907502+elizabeth-ko@users.noreply.github.com>
2024-03-19 14:33:18 -0400

    Update book_list_database.sql

--- SQL/book_list_database.sql
+++ SQL/book_list_database.sql
@@ -1,4 +1,4 @@
-/** Favorite Books:
+/** Store favorite books in a table:
 Breakfast at Tiffinay's
 Stand Your Ground
 Lincoln on Leadership
@@ -9,4 +9,6 @@ CREATE TABLE Favorite_Books ( id INTEGER PRIMARY KEY, name TEXT, rating INTEGER 
 INSERT INTO Favorite_Books VALUES (1, "Breakfast at Tiffinay's", 1);
 INSERT INTO Favorite_Books VALUES (2, "Stand Your Ground", 3);
 INSERT INTO Favorite_Books VALUES (3, "Lincoln On Leadership", 2);
-SELECT * FROM Favorite_Books
+
+/** Query the data and select everything. **/ 
+SELECT * FROM Favorite_Books;
